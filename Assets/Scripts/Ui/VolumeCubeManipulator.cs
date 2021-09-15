42a820c9519c7dc7a3b5060f454049ec3f87fa32
jon škoberne <jonchisko@gmail.com>
2021-09-15 18:56:07 +0200

    Add slider rotation functionality to volume cube
    Since hand manipulation could prove to be obnoxious, perhaps using sliders will help.
    These don't actually determine the final rotation but control the speed of the rotation around a particular world axis.

---
+++ Assets/Scripts/Ui/VolumeCubeManipulator.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.MixedReality.Toolkit.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeCubeManipulator : MonoBehaviour
+{
+    [SerializeField]
+    private Transform volumeCube;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnSliderUpdateRotX(SliderEventData data)
+    {
+        //volumeCube.rotation = Quaternion.identity;
+        volumeCube.RotateAround(volumeCube.position, Vector3.right, data.NewValue * Time.deltaTime);
+    }
+
+    public void OnSliderUpdateRotY(SliderEventData data)
+    {
+        //volumeCube.rotation = Quaternion.identity;
+        volumeCube.RotateAround(volumeCube.position, Vector3.up, data.NewValue * Time.deltaTime);
+    }
+
+    public void OnSliderUpdateRotZ(SliderEventData data)
+    {
+        //volumeCube.rotation = Quaternion.identity;
+        volumeCube.RotateAround(volumeCube.position, Vector3.forward, data.NewValue * Time.deltaTime);
+    }
+
+    public void ResetRotation()
+    {
+        volumeCube.rotation = Quaternion.identity;
+    }
+}
