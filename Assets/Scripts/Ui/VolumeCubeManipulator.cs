07ff52579e52f9628d1b50d3077f0f43e6662b51
jon škoberne <jonchisko@gmail.com>
2021-09-16 14:38:18 +0200

    Vol Manipulator: Add freeze functionality

--- Assets/Scripts/Ui/VolumeCubeManipulator.cs
+++ Assets/Scripts/Ui/VolumeCubeManipulator.cs
@@ -8,6 +8,8 @@ public class VolumeCubeManipulator : MonoBehaviour
     [SerializeField]
     private Transform volumeCube;
 
+    private bool freeze = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,46 +19,59 @@ public class VolumeCubeManipulator : MonoBehaviour
     public void OnSliderUpdateRotX(SliderEventData data)
     {
         //volumeCube.rotation = Quaternion.identity;
-        volumeCube.RotateAround(volumeCube.position, Vector3.right, data.NewValue * Time.deltaTime);
+        if (!freeze) volumeCube.RotateAround(volumeCube.position, Vector3.right, data.NewValue * Time.deltaTime);
     }
 
     public void OnSliderUpdateRotY(SliderEventData data)
     {
         //volumeCube.rotation = Quaternion.identity;
-        volumeCube.RotateAround(volumeCube.position, Vector3.up, data.NewValue * Time.deltaTime);
+        if (!freeze) volumeCube.RotateAround(volumeCube.position, Vector3.up, data.NewValue * Time.deltaTime);
     }
 
     public void OnSliderUpdateRotZ(SliderEventData data)
     {
         //volumeCube.rotation = Quaternion.identity;
-        volumeCube.RotateAround(volumeCube.position, Vector3.forward, data.NewValue * Time.deltaTime);
+        if (!freeze) volumeCube.RotateAround(volumeCube.position, Vector3.forward, data.NewValue * Time.deltaTime);
     }
 
     public void OnSliderUpdateScale(SliderEventData data)
     {
         Vector3 scaleChange = Vector3.one * data.NewValue * Time.deltaTime;
         Vector3 result = volumeCube.localScale + scaleChange;
-        if (result.x < 0 || result.y < 0 || result.z < 0)
+        if (!freeze)
         {
-            volumeCube.localScale = Vector3.zero;
-        } else
-        {
-            volumeCube.localScale = result;
+            if (result.x < 0 || result.y < 0 || result.z < 0)
+            {
+                volumeCube.localScale = Vector3.zero;
+            }
+            else
+            {
+                volumeCube.localScale = result;
+            }
         }
     }
 
     public void ResetScale()
     {
-        volumeCube.localScale = Vector3.one;
+        if (!freeze) volumeCube.localScale = Vector3.one;
     }
 
     public void ResetPosition()
     {
-        volumeCube.position = Vector3.zero;
+        if (!freeze) volumeCube.position = Vector3.zero;
     }
 
     public void ResetRotation()
     {
-        volumeCube.rotation = Quaternion.identity;
+        if (!freeze) volumeCube.rotation = Quaternion.identity;
+    }
+
+    public void ToggleFreeze()
+    {
+        freeze = !freeze;
+
+        volumeCube.GetComponent<MinMaxScaleConstraint>().enabled = freeze;
+        volumeCube.GetComponent<MoveAxisConstraint>().enabled = freeze;
+        volumeCube.GetComponent<RotationAxisConstraint>().enabled = freeze;
     }
 }
