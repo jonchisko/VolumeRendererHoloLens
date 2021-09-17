2bd3a172a9c4798aeef7bf4093aa0f436214e499
jon škoberne <jonchisko@gmail.com>
2021-09-17 15:28:05 +0200

    Add base for transfer function functionality

---
+++ Assets/Scripts/TransferFunctionDrawer/TransferFunctionPoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace com.jon_skoberne.TransferFunctionDrawer {
+    public class TransferFunctionPoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+}
+
