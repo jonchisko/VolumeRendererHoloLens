6178bab78fdcb1f9695eba01c9970aed8ea48706
jon škoberne <jonchisko@gmail.com>
2021-09-25 22:21:37 +0200

    Add menu UI items

---
+++ Assets/Scripts/Ui/MainMenuPopup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MainMenuPopup : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI titleText = null;
+    [SerializeField]
+    private TextMeshProUGUI contentText = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+    public void SetTitle(string text)
+    {
+        titleText.text = text;
+    }
+
+    public void SetContent(string text)
+    {
+        contentText.text = text;
+    }
+}
---
+++ Assets/Scripts/Ui/SkullMenuRotation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullMenuRotation : MonoBehaviour
+{
+    public float rotationSpeed = 1.0f;
+
+    private float zRotation = 0.0f;
+    private Vector3 oldRotation = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        oldRotation = this.transform.localRotation.eulerAngles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateSelf();
+    }
+
+    private void RotateSelf()
+    {
+        zRotation += rotationSpeed * Time.deltaTime;
+        zRotation = Mathf.Clamp(zRotation, -180.0f, 180.0f);
+        this.transform.localRotation = Quaternion.Euler(oldRotation.x, oldRotation.y, zRotation);
+    }
+}
