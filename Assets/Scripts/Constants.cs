d505f28e304e85c1c0a7be2a165bd0ef1a152bc8
jon škoberne <jonchisko@gmail.com>
2021-11-06 21:36:50 +0100

    Add constants

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Constants
+{
+    public static float maxUnitCubeLength = 1.75f;
+    public static float maxNormalisedMagnitude = 1.75f; // sqrt(1^2 + 1^2 + 1^2) = swrt(3) = a bit less than 1.75
+}
