5cd2e729a45323282e1a7f127b43ac9dd0939c45
jon škoberne <jonchisko@gmail.com>
2021-12-16 14:32:38 +0100

    Add feature: 3D map for selecting appropriate "histogram" area

---
+++ Assets/Scripts/TransferFunctionDrawer/Map3D.cs
@@ -0,0 +1,152 @@
+﻿using com.jon_skoberne.UI;
+using Microsoft.MixedReality.Toolkit.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map3D : MonoBehaviour
+{
+    public struct SelectedHistValues
+    {
+        public float pos_x;
+        public float pos_y;
+        public float pos_z;
+        public float filter_dim;
+
+        public SelectedHistValues(float pos_x, float pos_y, float pos_z, float filter_dim)
+        {
+            this.pos_x = pos_x;
+            this.pos_y = pos_y;
+            this.pos_z = pos_z;
+            this.filter_dim = filter_dim;
+        }
+    }
+
+    public delegate void OnSelectedMapRegion(SelectedHistValues s);
+    public static OnSelectedMapRegion onSelectedMapRegion;
+
+
+    public SliderLogic slider_pos_x;
+    public SliderLogic slider_pos_y;
+    public SliderLogic slider_pos_z;
+    public SliderLogic slider_filter_dim;
+
+
+    [SerializeField]
+    int map_dim = 25;
+    [SerializeField]
+    ComputeShader compute_filter;
+    [SerializeField]
+    GameObject map_cube;
+
+    RenderTexture map_rt;
+    Texture3D map_t3d;
+    float pos_x = 0.5f;
+    float pos_y = 0.5f;
+    float pos_z = 0.5f;
+    float filter_dim = 0.5f;
+
+    void Awake()
+    {
+        InitialMapSetup();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        RegisterSliderEvents();
+    }
+
+    void OnDisable()
+    {
+        UnregisterSliderEvents();
+    }
+
+    void OnDestroy()
+    {
+        ClearRenderTex();
+    }
+
+    void InitialMapSetup()
+    {
+        this.map_t3d = new Texture3D(this.map_dim, this.map_dim, this.map_dim, TextureFormat.RFloat, false);
+        this.map_t3d.wrapMode = TextureWrapMode.Clamp;
+
+        this.map_rt = new RenderTexture(this.map_dim, this.map_dim, 0, RenderTextureFormat.RFloat);
+        this.map_rt.dimension = UnityEngine.Rendering.TextureDimension.Tex3D;
+        this.map_rt.volumeDepth = this.map_dim;
+        this.map_rt.enableRandomWrite = true;
+        this.map_rt.useMipMap = false;
+        this.map_rt.wrapMode = TextureWrapMode.Clamp;
+        this.map_rt.Create();
+
+        this.map_cube.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_MainTex", this.map_t3d);
+
+        Draw3dMap();
+    }
+
+    void RegisterSliderEvents()
+    {
+        slider_pos_x.changedValue.AddListener(UpdateValues);
+        slider_pos_y.changedValue.AddListener(UpdateValues);
+        slider_pos_z.changedValue.AddListener(UpdateValues);
+        slider_filter_dim.changedValue.AddListener(UpdateValues);
+    }
+
+    void UnregisterSliderEvents()
+    {
+        slider_pos_x.changedValue.RemoveAllListeners();
+        slider_pos_y.changedValue.RemoveAllListeners();
+        slider_pos_z.changedValue.RemoveAllListeners();
+        slider_filter_dim.changedValue.RemoveAllListeners();
+    }
+
+    void ClearRenderTex()
+    {
+        this.map_rt.Release();
+        this.map_rt = null;
+    }
+
+    void UpdateValues(SliderEventData _slv)
+    {
+        Debug.Log("Map: updating values");
+        this.pos_x = slider_pos_x.CurrentValue();
+        this.pos_y = slider_pos_y.CurrentValue();
+        this.pos_z = slider_pos_z.CurrentValue();
+        this.filter_dim = slider_filter_dim.CurrentValue();
+
+        Draw3dMap();
+    }
+
+    void Draw3dMap()
+    {
+        Debug.Log("Map: Drawing in compute shader");
+        Draw();
+        onSelectedMapRegion?.Invoke(GetSelectedHistValues());
+    }
+
+    void Draw()
+    {
+        this.compute_filter.SetFloats("MapDims", this.map_dim, this.map_dim, this.map_dim);
+        this.compute_filter.SetFloats("DimValues", this.pos_x, this.pos_y, this.pos_z, this.filter_dim);
+        this.compute_filter.SetTexture(0, "Result", this.map_rt);
+        this.compute_filter.Dispatch(0, this.map_rt.width / 8 + 1, this.map_rt.height / 8 + 1, this.map_rt.volumeDepth / 8 + 1);
+        Graphics.CopyTexture(this.map_rt, this.map_t3d);
+    }
+
+    SelectedHistValues GetSelectedHistValues()
+    {
+        return new SelectedHistValues(this.pos_x, this.pos_y, this.pos_z, this.filter_dim);
+    }
+}
---
+++ Assets/Shaders/Map3dShader.shader
@@ -0,0 +1,121 @@
+﻿Shader "Unlit/Map3dShader"
+{
+    Properties
+    {
+        _MainTex ("Texture", 3D) = "white" {}
+    }
+    SubShader
+    {
+        Tags {"Queue" = "Transparent" "IgnoreProjector" = "True" "RenderType" = "Transparent"}
+        LOD 100
+        Cull Front
+        ZTest LEqual
+        ZWrite On
+        //Blend SrcAlpha OneMinusSrcAlpha
+        //ZWrite Off
+
+        // Cull Front/Back
+        Blend SrcAlpha OneMinusSrcAlpha
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            #define MAX_CUBE_DIST 1.732f
+            #define NUM_STEPS 128
+
+            struct appdata // vertex in 
+            {
+                float4 vertex : POSITION;
+                float4 normal: NORMAL;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f // fragment in
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+                float3 vertexLocal: TEXCOORD1;
+                float3 normal: NORMAL;
+                float3 vectorToSurface: TEXCOORD2;
+            };
+
+            sampler3D _MainTex;
+
+
+            float getDensity(float3 pos)
+            {
+                return tex3Dlod(_MainTex, float4(pos.xyz, 0.0f) + float4(0.5f, 0.5f, 0.5f, 0.0f)).r;
+            }
+
+
+            float3 getAbsFloat3(float3 position) {
+                return float3(abs(position.x), abs(position.y), abs(position.z));
+            }
+
+            float rand_1_05(float2 uv)
+            {
+                float2 noise = (frac(sin(dot(uv, float2(12.9898, 78.233) * 2.0)) * 43758.5453));
+                return abs(noise.x + noise.y) * 0.5;
+            }
+
+            float3 getOffsetPosition(v2f i, float3 dir) {
+                return i.vertexLocal + (2.0f * dir / NUM_STEPS) * rand_1_05(i.uv);
+            }
+
+
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                o.vertexLocal = v.vertex; // vertex in object space 
+                o.vectorToSurface = v.vertex - mul(unity_WorldToObject, float4(_WorldSpaceCameraPos, 1.0)); // ray direction is in object space
+                o.normal = UnityObjectToWorldNormal(v.normal);
+                return o;
+            }
+
+            fixed4 frag_mip(v2f i) : SV_Target
+            {
+                const float StepSize = MAX_CUBE_DIST / NUM_STEPS;
+                float3 rayDir = -normalize(i.vectorToSurface);
+                float3 rayStartPos = getOffsetPosition(i, rayDir);
+
+                float4 maxCol = float4(1.0f, 1.0f, 1.0f, 0.2f);
+                float3 maxPosition = float3(0.0f, 0.0f, 0.0f);
+                float maxDensity = 0.0f;
+                for (int iStep = 0; iStep < NUM_STEPS; iStep++) {
+                    const float t = iStep * StepSize;
+                    const float3 currentPos = rayStartPos + t * rayDir;
+                    float3 abs_xyz = getAbsFloat3(currentPos);
+                    if (abs_xyz.x > 0.5f || abs_xyz.y > 0.5f || abs_xyz.z > 0.5f) {
+                       continue;
+                    } // dont sample if we are outside the unit box bounds
+                    const float density = getDensity(currentPos);
+
+                    if (density > maxDensity) {
+                        maxDensity = density;
+                        maxCol = float4(1.0f, 0.0f, 0.0f, maxDensity);
+                        maxDensity = density;
+                        maxPosition = currentPos;
+                    }
+                }
+                return maxCol;
+            }
+
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = frag_mip(i);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
